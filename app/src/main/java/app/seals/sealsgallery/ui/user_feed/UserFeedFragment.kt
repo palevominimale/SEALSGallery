c395f4d87fdbeb393bfb2ed651f2c662a201b256
palevominimale <badtri.orchestra@gmail.com>
2022-11-01 12:49:45 +0100

    added load user info from firebase

--- app/src/main/java/app/seals/sealsgallery/ui/user_feed/UserFeedFragment.kt
+++ app/src/main/java/app/seals/sealsgallery/ui/user_feed/UserFeedFragment.kt
@@ -6,11 +6,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import app.seals.sealsgallery.R
 import app.seals.sealsgallery.ui.adapters.FeedRecyclerAdapter
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class UserFeedFragment : Fragment() {
@@ -30,12 +38,48 @@ class UserFeedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recycler = view.findViewById<RecyclerView>(R.id.userpageRecyclerView)
+        val userAvatar = view.findViewById<ImageView>(R.id.userpageAvatar)
+        val userName = view.findViewById<TextView>(R.id.userpageUsername)
+        val userSubscribers = view.findViewById<TextView>(R.id.userpageSubscribers)
+        val userSubscriptions = view.findViewById<TextView>(R.id.userpageSubscriptions)
+        val userTracksNumber = view.findViewById<TextView>(R.id.userpageTotalTracks)
         val adapter = FeedRecyclerAdapter(vm.userFeed)
+
         recycler.adapter = adapter
         recycler.layoutManager = LinearLayoutManager(requireContext())
         vm.loadFeed()
+        vm.loadUserData()
+
         vm.userFeed.observe(viewLifecycleOwner) {
             adapter.notifyDataSetChanged()
+        }
+        vm.userData.observe(viewLifecycleOwner) {
+            Log.e("MTFF", "$it")
+            var userPhoto : RoundedBitmapDrawable? = null
+            CoroutineScope(Dispatchers.IO).launch {
+                val bitmap = kotlin.runCatching {
+                    Picasso.get().load(it.photoLink).get()
+                }
+                val bitmapGet = bitmap.getOrNull()
+                userPhoto = if(bitmapGet != null) {
+                    RoundedBitmapDrawableFactory.create(requireContext().resources, bitmapGet)
+                        .apply {
+                            isCircular = true
+                        }
+                } else null
+            }.invokeOnCompletion {
+                requireActivity().runOnUiThread {
+                    if (userPhoto != null) {
+                        userAvatar.setImageDrawable(userPhoto)
+                    } else {
+                        userAvatar.setImageResource(R.drawable.ic_baseline_person_24)
+                    }
+                }
+            }
+            userName.text = it.name
+            userSubscriptions.text = it.listOfSubscriptions.size.toString()
+            userSubscribers.text = it.listOfSubscribers.size.toString()
+            userTracksNumber.text = vm.userFeed.value?.size.toString()
         }
     }
 }
--- app/src/main/java/app/seals/sealsgallery/ui/user_feed/UserFeedViewModel.kt
+++ app/src/main/java/app/seals/sealsgallery/ui/user_feed/UserFeedViewModel.kt
@@ -3,9 +3,12 @@ package app.seals.sealsgallery.ui.user_feed
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import app.seals.sealsgallery.R
 import app.seals.sealsgallery.domain.interfaces.FeedRepository
 import app.seals.sealsgallery.domain.models.PostDomainModel
+import app.seals.sealsgallery.domain.models.UserDomainModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class UserFeedViewModel(
     context: Context,
@@ -13,8 +16,26 @@ class UserFeedViewModel(
 ) : ViewModel() {
 
     val userFeed = MutableLiveData<List<PostDomainModel>>()
+    val userData = MutableLiveData<UserDomainModel>()
+    private val refMainNode = context.getString(
+        R.string.firebase_reference_name)
+    private val refTracksNode = context.getString(R.string.firebase_reference_tracks_name)
+    private val refUserDataNode = context.getString(R.string.firebase_reference_user_data)
+    private val db = FirebaseDatabase.getInstance()
     private val userFeedInternal = mutableListOf<PostDomainModel>()
     val auth = FirebaseAuth.getInstance()
+    private val ref = db.getReference(refMainNode)
+    private val refUserData = ref.child(auth.uid.toString()).child(refUserDataNode)
+
+    fun loadUserData() {
+        refUserData.get().addOnCompleteListener { snapshot ->
+            userData.postValue(snapshot.result.getValue(UserDomainModel::class.java))
+        }
+    }
+
+    fun loadUserDataByUid(uid: String) {
+        val refUserById = ref.child(uid).child(refUserDataNode)
+    }
 
     fun loadFeed() {
         userFeedInternal.clear()
